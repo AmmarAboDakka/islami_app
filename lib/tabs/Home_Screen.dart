2cb59fe1383bc1a4898901a8cf6f76a42f86ffdc
Ammar <ammar.dakka@gmail.com>
2022-11-26 20:03:32 +0300

    Create Radio Tab

--- lib/tabs/Home_Screen.dart
+++ lib/tabs/Home_Screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:islami_application/My_Theme.dart';
-import 'package:islami_application/tabs/hadeth_tab.dart';
+import 'package:islami_application/ahades/hadeth_tab.dart';
 import 'package:islami_application/quran/quran_tab.dart';
-import 'package:islami_application/tabs/radio_tab.dart';
+import 'package:islami_application/radio/radio_tab.dart';
 import 'package:islami_application/sebha/tasbeh_tab.dart';
 
 class HomeScreen extends StatefulWidget {
